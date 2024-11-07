3b96d0ff70c33672eb1c2b57e95c1d62a6ac9c66
VasylDvorakDS <vasyldvorak@yanex.ru>
2024-11-07 16:28:49 +0300

    parsing from api.foursquare.com update third

--- Task_1/Task_1_at_home/foursquare.py
+++ Task_1/Task_1_at_home/foursquare.py
@@ -14,10 +14,13 @@ while True:
     print()
     query = str(input("Введите заведение, которое хотите найти = "))
     limit = int(input("Введите лимит запроса от 1 до 50 = "))
+    city = str(input("Введите название города = "))
 
     params={
     "query":query,
     "limit":limit,
+    "near": city,
+    "fields": 'name,location,rating',
     "sort":"RATING"}
     response = requests.get(url, headers=headers, params=params)
 
@@ -25,9 +28,9 @@ while True:
         print('\n','СПИСОК МЕСТ:','\n')
         j_data=response.json()
         for item, place in enumerate(j_data.get('results')):
-            print('Имя заведения: ',place.get('name'))
-            print('Рейтинг: ', item+1)
-            print('Адрес заведения:  ', place.get('location').get('formatted_address'),'\n')
+            print(item+1,') Имя заведения: ',place.get('name'))
+            print('Адрес заведения:  ', place.get('location').get('formatted_address'))
+            print('Рейтинг: ', place.get('rating') if 'rating' in place else "Рейтинг не определялся", '\n')
     else:
         print("Ошибка сервера")
 
